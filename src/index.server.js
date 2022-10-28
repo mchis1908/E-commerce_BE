0ee326895125b89f070d3aa61ac5fd70070eb5ac
Thiện Khải <thienkhaiak5555@gmail.com>
2022-10-28 22:49:28 +0700

    Tạo product api

--- src/index.server.js
+++ src/index.server.js
@@ -8,6 +8,7 @@ const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
 const adminRoutes = require('./routes/admin/auth');
 const categoryRoutes = require('./routes/category'); //Định tuyến category
+const productRoutes = require('./routes/product'); //Định tuyến product
 
 //environment variable
 env.config();
@@ -24,6 +25,7 @@ app.use(express.json())
 app.use("/api", authRoutes);
 app.use("/api", adminRoutes);
 app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
 
 
 
