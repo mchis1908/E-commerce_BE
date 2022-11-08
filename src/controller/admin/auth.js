5e1946b4f0ee198406da765d3f7d08dcb1b96473
hungnongtrai <vuquochung2204@gmail.com>
2022-11-08 14:09:50 +0700

    Submit #3
    1.Api product

--- src/controller/admin/auth.js
+++ src/controller/admin/auth.js
@@ -1,8 +1,9 @@
 const User = require('../../models/user');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 exports.signup = (req, res) => {
     User.findOne({ email: req.body.email })
-        .exec((error, user) => {
+        .exec(async (error, user) => {
             if (user) return res.status(400).json({
                 message: "Admin already registered"
             });
@@ -13,11 +14,12 @@ exports.signup = (req, res) => {
                 email,
                 password
             } = req.body;
+            const hash_password = await bcrypt.hash(password, 10);
             const _user = new User({
                 firstName,
                 lastName,
                 email,
-                password,
+                hash_password,
                 userName: Math.random().toString(),
                 role: 'admin'
             });
@@ -44,9 +46,9 @@ exports.signin = (req, res) => {
             if (error) return res.status(400).json({ error });
             if (user) {
                 if (user.authenticate(req.body.password) && user.role === 'admin') {
-                    const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '100d' });
+                    const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1d' });
                     const { _id, firstName, lastName, email, role, fullName } = user;
-                    res.cookie('token', token, { expiresIn: '100d' });
+                    res.cookie('token', token, { expiresIn: '1d' });
                     res.status(200).json({
                         token,
                         user: { _id, firstName, lastName, email, role, fullName }
--- src/index.server.js
+++ src/index.server.js
@@ -12,6 +12,7 @@ const adminRoutes = require('./routes/admin/auth');
 const categoryRoutes = require('./routes/category'); //Định tuyến category
 const productRoutes = require('./routes/product'); //Định tuyến product
 const cartRoutes = require('./routes/cart'); //Route cho phần Cart
+const initialDataRoutes = require('./routes/admin/initialData');
 
 //environment variable
 env.config();
@@ -31,6 +32,8 @@ app.use("/api", adminRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
 app.use("/api", cartRoutes);
+app.use("/api", initialDataRoutes);
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
