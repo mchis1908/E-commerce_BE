7a1731e251260ca525889ffb6b323d7515c036ad
hungnongtrai <vuquochung2204@gmail.com>
2022-11-24 10:26:38 +0700

    Submit #8
    1. Order API

--- src/index.server.js
+++ src/index.server.js
@@ -1,33 +1,36 @@
 const express = require("express");
 const env = require("dotenv");
 const app = express();
-const mongoose = require('mongoose');
-const path = require('path')
-const cors = require('cors')
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
 
 //routes
 
-const authRoutes = require('./routes/auth');
-const adminRoutes = require('./routes/admin/auth');
-const categoryRoutes = require('./routes/category'); //Định tuyến category
-const productRoutes = require('./routes/product'); //Định tuyến product
-const cartRoutes = require('./routes/cart'); //Route cho phần Cart
-const initialDataRoutes = require('./routes/admin/initialData');
-const pageRoutes = require('./routes/admin/page');
-const addressRoutes = require('./routes/address');
-
+const authRoutes = require("./routes/auth");
+const adminRoutes = require("./routes/admin/auth");
+const categoryRoutes = require("./routes/category"); //Định tuyến category
+const productRoutes = require("./routes/product"); //Định tuyến product
+const cartRoutes = require("./routes/cart"); //Route cho phần Cart
+const initialDataRoutes = require("./routes/admin/initialData");
+const pageRoutes = require("./routes/admin/page");
+const addressRoutes = require("./routes/address");
+const orderRoutes = require("./routes/order");
 //environment variable
 env.config();
 
 // mongodb connect
 //mongodb+srv://root:<password>@cluster0.dun0jsb.mongodb.net/?retryWrites=true&w=majority
-mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.dun0jsb.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`)
-    .then(() => {
-        console.log("Database connected")
-    });
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.dun0jsb.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Database connected");
+  });
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 app.use("/public", express.static(path.join(__dirname, "uploads")));
 app.use("/api", authRoutes);
 app.use("/api", adminRoutes);
@@ -37,7 +40,8 @@ app.use("/api", cartRoutes);
 app.use("/api", initialDataRoutes);
 app.use("/api", pageRoutes);
 app.use("/api", addressRoutes);
+app.use("/api", orderRoutes);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
