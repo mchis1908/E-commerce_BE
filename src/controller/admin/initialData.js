a792b97c19b44b3dfefd0101ad9fb5e1b3ce5fcb
hungnongtrai <vuquochung2204@gmail.com>
2023-01-06 14:26:25 +0700

    Sale

--- src/controller/admin/initialData.js
+++ src/controller/admin/initialData.js
@@ -28,7 +28,9 @@ function createCategories(categories, parentId = null) {
 exports.initialData = async (req, res) => {
   const categories = await Category.find({}).exec();
   const products = await Product.find({})
-    .select("_id name price quantity slug description productPictures category")
+    .select(
+      "_id name price quantity slug description productPictures category sale"
+    )
     .populate({ path: "category", select: "_id name" })
     .exec();
   const orders = await Order.find({})
